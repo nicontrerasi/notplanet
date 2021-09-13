5c3c60c586620f2e925d84720324dbea5efad293
nicontrerasi <nico.contrerasi@duocuc.cl>
2021-09-13 18:36:02 -0300

    Creacion y modificacion de page Configuracion y Mi Perfil de APP

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'inicio',
+    pathMatch: 'full'
   },
   {
     path: 'inicio',
